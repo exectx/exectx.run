b1865c2fd6002cb114609ba19b3ca7216c4b8ea9
hal <me@exectx.run>
2024-09-15 21:10:18 -0500

    feat: update wrangler

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,10 +3,24 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        mono: ["JetBrainsMono", ...fontFamily.mono],
+        mono: ["IBM Plex Mono"],
+        sans: ["IBM Plex Sans"],
+        serif: ["IBM Plex Serif"],
+      },
+      keyframes: {
+        "ping-2": {
+          "75%, 100%": {
+            transform: "scale(3)",
+            opacity: 0,
+          },
+        },
+      },
+      animation: {
+        "ping-2": "ping-2 2s cubic-bezier(0, 0, 0.2, 1) infinite",
       },
     },
   },
